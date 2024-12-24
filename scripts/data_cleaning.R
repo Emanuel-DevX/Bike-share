15cd5dcd2b85b29e8c70db0f7bc7b4dcae13bdd7
Emanuel-DevX <jefymoxi@gmail.com>
2024-12-24 09:24:43 +0000

    Loaded the data to a local R variable

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -1,3 +1,10 @@
 
 install.packages('tidyverse')
 library(tidyverse)
+data_2019 <- read.csv("data/raw/Divvy_Trips_2019_Q1.csv")
+head(data_2019)
+View(data_2019)
+
+data_2020 <- read.csv("data/raw/Divvy_Trips_2020_Q1.csv")
+head(data_2020)
+View(data_2020)
