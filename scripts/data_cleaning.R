88bf01e170ab85e6bc6871f8d960dfbc4a6768c8
Emanuel-DevX <jefymoxi@gmail.com>
2024-12-26 05:13:12 +0000

    renamed the name of the attributes for  consistency

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -8,3 +8,19 @@ View(data_2019)
 data_2020 <- read.csv("data/raw/Divvy_Trips_2020_Q1.csv")
 head(data_2020)
 View(data_2020)
+
+colnames(data_2019)  
+colnames(data_2020)
+
+data_2019 <- rename(data_2019, 
+                  ride_id = trip_id,
+                  rideable_type = bikeid,
+                  started_at = start_time,
+                  ended_at = end_time,
+                  start_station_name = from_station_name,
+                  start_station_id = from_station_id,
+                  end_station_name = to_station_name,
+                  end_station_id = to_station_id,
+                  member_casual = usertype)
+
+
